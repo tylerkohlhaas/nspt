6f154f29287e97e36b1acea46be420132c2d73eb
tylerkohlhaas <tylerkohlhaas@users.noreply.github.com>
2025-08-24 10:04:06 -0700

    Updated nav, services, about pages

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,131 +1,39 @@
-import { getPermalink, getBlogPermalink } from './utils/permalinks';
+import { getPermalink, getAsset } from './utils/permalinks';
 
 export const headerData = {
   links: [
-    /*{
-      text: 'Homes',
+    {
+      text: 'Services',
+      href: getPermalink('/services'),
       links: [
-        {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
-          href: getPermalink('/homes/personal'),
-        },
+         { text: 'Academic Support', href: '#academic-support' } ,
+         { text: 'Behaviour Consultation', href: '#behaviour-consultation' }
       ],
+    },
+    {
+      text: 'About',
+      href: getPermalink('/about'),
+    },
+    /*{
+      text: 'Blog',
+      href: getBlogPermalink(),
     },*/
     {
-      text: 'Pages',
-      links: [
-        {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
-        },
-        {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
-          href: getPermalink('/terms'),
-        },
-        {
-          text: 'Privacy policy',
-          href: getPermalink('/privacy'),
-        },
-      ],
-    },
-    {
-      text: 'Landing',
-      links: [
-        {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
-        },
-        {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
-        },
-        {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
-        },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
-      ],
-    },
-    {
-      text: 'Blog',
-      links: [
-        {
-          text: 'Blog List',
-          href: getBlogPermalink(),
-        },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
-      ],
-    },
-    {
-      text: 'Widgets',
-      href: '#',
+      text: 'Contact',
+      href: getPermalink('/contact'),
     },
   ],
-  actions: [{ text: 'Contact Us', href: '#', target: '_blank' }], // TODO: Change to contact page
+  actions: [{ text: 'Free Consultation', href: getPermalink('/contact'), target: '_self' }],
 };
+
 
 export const footerData = {
   links: [
     {
       title: 'Services',
       links: [
-        { text: 'Behaviour Consultation', href: '#' },
-        { text: 'Reading Support', href: '#' },
-        { text: 'Foundational Learning Skills', href: '#' },
+        { text: 'Academic Support', href: '#academic-support' } ,
+        { text: 'Behaviour Consultation', href: '#behaviour-consultation' }
       ],
     },
     /*{
@@ -155,14 +63,12 @@ export const footerData = {
     { text: 'Terms', href: getPermalink('/terms') },
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
-  /*socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
+  socialLinks: [
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/arthelokyo/astrowind' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 underline dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
-  `,*/
+    North Shore Precision Teaching · All rights reserved.
+  `,
 };
